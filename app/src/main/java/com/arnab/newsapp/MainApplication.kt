ea202ad8d8aa08b8f222d5c742608f03e44af6af
arnab-shanta-anu <arnab.shanta@bjitacademy.com>
2023-01-17 09:11:18 +0600

    initial commit

---
+++ app/src/main/java/com/arnab/newsapp/MainApplication.kt
@@ -0,0 +1,16 @@
+package com.arnab.newsapp
+
+import android.app.Application
+import android.content.Context
+
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        MyApplication.appContext = applicationContext
+    }
+
+    companion object {
+        lateinit  var appContext: Context
+    }
+}
